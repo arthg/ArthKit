3c99ec4110cb0a9b323d31aa08dc6d94f4625d10
Arthur Gorr <art.gorr@gmail.com>
2016-12-27 07:34:25 -0600

    delete user

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,13 +4,27 @@ import getBaseUrl from './baseUrl';
 
 const baseUrl = getBaseUrl();
 
+//STRIP_ON_CLONE
 // the rest of the funtions here are private since no export
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import { getUsers, deleteUser } from './api/userApi';
 
+//STRIP_ON_CLONE
 // Populate table of users via API call.
 getUsers().then(result => {
   let usersBody = "";
@@ -19,4 +20,17 @@ getUsers().then(result => {
   // in a real app will use Angualr or React or ..
   global.document.getElementById('users').innerHTML = usersBody;
 
+const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
